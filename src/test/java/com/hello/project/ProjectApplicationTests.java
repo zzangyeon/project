f5e03a9d50205da9a5006d04d7e8a5d2e0b0d3cf
zzangyeon <zxkim2004@gmail.com>
2021-09-30 15:43:19 +0900

    테스트코드 수정

--- src/test/java/com/hello/project/ProjectApplicationTests.java
+++ src/test/java/com/hello/project/ProjectApplicationTests.java
@@ -13,29 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 
 class ProjectApplicationTests {
-
-	@Autowired
-	UserRepository userRepository;
-	@Autowired
-	SubscribeRepository subscribeRepository;
-
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void userEntity(){
-		User user = new SignupDto("지연", "1234", "zxkim2004@naver.com", "최지연").toEntity();
-		User userEntity = userRepository.save(user);
-		System.out.println(userEntity);
-	}
-
-	@Test
-	void countSubscribeState(){
-
-		int state = subscribeRepository.subscribeState(6L,1L);
-		System.out.println("state = " + state);
-	}
-
+	
 
 }
