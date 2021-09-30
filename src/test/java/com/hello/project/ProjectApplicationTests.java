90259f59e17761d8280710c508d62dddf56fc688
zzangyeon <zxkim2004@gmail.com>
2021-09-30 16:07:22 +0900

    테스트코드 수정

--- src/test/java/com/hello/project/ProjectApplicationTests.java
+++ src/test/java/com/hello/project/ProjectApplicationTests.java
@@ -8,13 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectApplicationTests {
 
-    @Test
-    public void test() {
-
-        ArticleDto articleDto = new ArticleDto();
-        String title = articleDto.getTitle();
-        Assertions.assertThat(title).isEqualTo("hello");
-
-    }
-
 }
